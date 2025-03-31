f4e7a25a7044dc430277bd642a8b5deb8e7000ad
0x8D <mirocairo15@gmail.com>
2025-03-31 19:03:27 +0000

    Integrate Mantine components, and configure PostCSS for enhanced styling in the application.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import path from "path"
+import react from "@vitejs/plugin-react"
+//@ts-expect-error - Missing type definitions for vite-plugin-eslint
+import eslint from 'vite-plugin-eslint';
+import { defineConfig } from "vite"
+ 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),eslint()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
